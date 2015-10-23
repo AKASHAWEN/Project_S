ff509ae4a148d4b573b368dd996ead8853659c53
Khan <ycchencoding@gmail.com>
2015-10-23 17:26:33 -0400

    add file to custom favorite table cell; add tableview into favorite control;

--- Project_S/FavouriteViewController.swift
+++ Project_S/FavouriteViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class FavouriteViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
